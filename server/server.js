a15cfef6cf591aa4a946fc91706d27b4172b9053
Gourang12332 <gourangjain782@gmail.com>
2025-11-15 11:55:04 +0530

    Origin Fix

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: [process.env.FRONTEND_URL , "http://localhost:3000"],
   credentials: true
 }));
 app.use(express.json());
